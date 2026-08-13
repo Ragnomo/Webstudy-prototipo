33bfa9363332d5211c20180e0a15253f238d1ab3
Ragnomo <155869829+Ragnomo@users.noreply.github.com>
2026-08-12 21:50:33 -0300

    urls.py

--- webstudy_project/urls.py
+++ webstudy_project/urls.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # Rota padrão do painel de administração do Django
+    
     path('admin/', admin.site.urls),
     
-    # Inclui todas as rotas do nosso app "core" na raiz do site
+    
     path('', include('core.urls')),
 ]
