61f18b8faa4b78a5fa5732adeab037d749b2b079
Ragnomo <155869829+Ragnomo@users.noreply.github.com>
2026-08-12 21:53:33 -0300

    urls.py

--- core/urls.py
+++ core/urls.py
@@ -2,15 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Rota da página inicial (Dashboard)
     path('', views.dashboard, name='dashboard'),
     
-    # Rota para ver os detalhes de um curso específico
     path('course/<int:pk>/', views.course_detail, name='course_detail'),
     
-    # Rota para acessar a tela dividida da aula
     path('lesson/<int:pk>/', views.lesson_detail, name='lesson_detail'),
     
-    # Rota invisível (API) que o JavaScript acessa para salvar o progresso
     path('api/complete/', views.mark_lesson_complete, name='api_complete'),
 ]
