0290e1b81b620dee629a1c4f10a9c950da6c3d32
Ragnomo <155869829+Ragnomo@users.noreply.github.com>
2026-08-12 21:52:09 -0300

    admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Course, Module, Lesson, Exercise, QuizQuestion, UserProgress
 
-# Registrando todos os modelos criados para aparecerem no painel admin
 admin.site.register(Course)
 admin.site.register(Module)
 admin.site.register(Lesson)
